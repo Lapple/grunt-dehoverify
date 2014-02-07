61588dfbc3c9c31c151dcdf02529476ee6893366
lapple <lapple@yandex-team.ru>
2014-02-07 18:39:58 +0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,62 @@
+/*
+ * grunt-dehoverify
+ * https://github.com/lapple/grunt-dehoverify
+ *
+ * Copyright (c) 2014 Aziz Yuldashev
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>',
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+      },
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp'],
+    },
+
+    // Configuration to be run (and then tested).
+    dehoverify: {
+      sample: {
+        files: {
+          'tmp/style.css': ['test/fixtures/style.css'],
+        }
+      },
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js'],
+    },
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'dehoverify', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
---
+++ tasks/dehoverify.js
@@ -0,0 +1,59 @@
+/*
+ * grunt-dehoverify
+ * https://github.com/lapple/grunt-dehoverify
+ *
+ * Copyright (c) 2014 Aziz Yuldashev
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var rework = require('rework');
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('dehoverify', function() {
+    var options = this.options({});
+
+    // Iterate over all specified file groups.
+    this.files.forEach(function(f) {
+      // Concat specified files.
+      var src = f.src.filter(function(filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function(filepath) {
+        var css = grunt.file.read(filepath);
+
+        return rework(css).use(dehoverify).toString({ compress: true });
+      }).join(grunt.util.lineFeed);
+
+      // Write the destination file.
+      grunt.file.write(f.dest, src);
+
+      // Print a success message.
+      grunt.log.writeln('File "' + f.dest.cyan + '" created.');
+    });
+  });
+
+};
+
+function dehoverify(style) {
+  style.rules = style.rules.map(function(rule) {
+    if (!rule.selectors) return rule;
+
+    rule.selectors = rule.selectors.filter(function(selector) {
+      return !(/\:hover/.test(selector));
+    });
+
+    if (rule.selectors.length === 0) {
+      rule.declarations = [];
+    }
+
+    return rule;
+  });
+}
